e6ff1ba95c7314d6e3250c5fb6bf8c6cb5c02688
Mohit kumar <mohitdcsa@gmail.com>
2025-06-27 19:52:23 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import Experiment from "./experiment.jsx";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function App(){
-  
-  return (<>
-<BrowserRouter>
-  <nav>
-    <Link to="/">
-    hello
-    </Link>
-  </nav>
-  <Routes>
-    <Route path="/" element={<Experiment/>}></Route>
-  </Routes>
-</BrowserRouter>
-  </>);
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">hello</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Experiment />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
